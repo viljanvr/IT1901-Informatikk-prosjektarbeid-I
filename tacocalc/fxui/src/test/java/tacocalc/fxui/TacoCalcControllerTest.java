e1f1e28a4083453f9933546eade6dfa996309fa9
Jonas Wolla <wolla.jonas@gmail.com>
2022-09-22 17:58:48 +0200

    Gjorde controllertesten til en TODO

--- tacocalc/fxui/src/test/java/tacocalc/fxui/TacoCalcControllerTest.java
+++ tacocalc/fxui/src/test/java/tacocalc/fxui/TacoCalcControllerTest.java
@@ -1,30 +1,9 @@
 package tacocalc.fxui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
 public class TacoCalcControllerTest extends ApplicationTest{
 
-    AppController controller;
-
-    @Override
-    public void start(final Stage stage) throws Exception {
-        final FXMLLoader loader = new FXMLLoader(getClass().getResource("ShoppingList.fxml"));
-        final Parent root = loader.load();
-        this.controller = new AppController();
-        stage.setScene(new Scene(root));
-        stage.show();
-     }
-
-    @Test
-    public void testAdditem() {
-        clickOn("#ingredientAmntField").write("3");
-        clickOn("#ingredientNameField").write("tomat");
-        clickOn("#addingredient");
-    }
+    // TODO: make tests for GUi using Applicationtest
+    
 }
