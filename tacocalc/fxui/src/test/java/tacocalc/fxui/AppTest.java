50af4db9f3d72dd5362e38e1fec5b77ff199bef4
Jonas Wolla <wolla.jonas@gmail.com>
2022-10-13 20:50:39 +0200

    Made test for IngredientEditController

--- tacocalc/fxui/src/test/java/tacocalc/fxui/AppTest.java
+++ tacocalc/fxui/src/test/java/tacocalc/fxui/AppTest.java
@@ -1,19 +1,20 @@
 package tacocalc.fxui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import tacocalc.core.Ingredient;
-import tacocalc.core.Recipe;
-import tacocalc.data.TacoCalcFileHandler;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+import tacocalc.core.Ingredient;
+import tacocalc.core.Recipe;
+import tacocalc.data.TacoCalcFileHandler;
 
 public class AppTest extends ApplicationTest {
   Parent root;
@@ -21,7 +22,9 @@ public class AppTest extends ApplicationTest {
 
   @Override
   public void start(final Stage stage) throws Exception {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("ShoppingList.fxml"));
+    FXMLLoader loader = new FXMLLoader(
+      getClass().getResource("ShoppingList.fxml")
+    );
     Parent root = loader.load();
     stage.setScene(new Scene(root));
     controller = loader.getController();
@@ -38,28 +41,61 @@ public class AppTest extends ApplicationTest {
     clickOn("#newIngredientNameField").write("agurk");
 
     // Checks if textfield contains expected text
-    Assertions.assertEquals("4", controller.getNewIngredientAmntField().getText());
-    Assertions.assertEquals("agurk", controller.getNewIngredientNameField().getText());
+    Assertions.assertEquals(
+      "4",
+      controller.getNewIngredientAmntField().getText()
+    );
+    Assertions.assertEquals(
+      "agurk",
+      controller.getNewIngredientNameField().getText()
+    );
     clickOn("#addIngredient");
     Assertions.assertEquals("4x agurk", getIngredientTextField(0));
 
     // Checks that the input fiels are cleared after adding an ingredient
-    Assertions.assertEquals("", controller.getNewIngredientAmntField().getText());
-    Assertions.assertEquals("", controller.getNewIngredientNameField().getText());
+    Assertions.assertEquals(
+      "",
+      controller.getNewIngredientAmntField().getText()
+    );
+    Assertions.assertEquals(
+      "",
+      controller.getNewIngredientNameField().getText()
+    );
 
     // Cheks that a second ingredient is added at the correct position
     clickOn("#newIngredientAmntField").write("5");
     clickOn("#newIngredientNameField").write("tomtat");
     clickOn("#addIngredient");
-    Assertions.assertEquals("5x tomtat", getIngredientTextField(1));
+    Assertions.assertEquals("5x tomat", getIngredientTextField(1));
   }
 
   // Tests the error message for wrong input in integer field
   @Test
-  @DisplayName("Test that adding an ingredient with an invalid amount doesn't do")
+  @DisplayName(
+    "Test that adding an ingredient with an invalid amount doesn't do"
+  )
   public void testInvalidAmount() {
     addIngredient("NotAnInteger", "Should give popup error");
     Assertions.assertEquals(0, controller.getIngredientViewStream().count());
+  }
+
+  // Tests Functions in the ingredient edit controller
+  @Test
+  @DisplayName(
+    "Test that you can increase and decrease the amount of an ingredient"
+  )
+  public void testIncreaseAndDecrease() {
+    addIngredient("3", "paprika");
+    clickOn("#editButton");
+    clickOn(getIngredientEditButton(0));
+    clickOn("#decreaseButton");
+    clickOn("#saveButton");
+    Assertions.assertEquals("2x paprika", getIngredientTextField(0));
+    clickOn(getIngredientEditButton(0));
+    clickOn("#increaseButton");
+    clickOn("#increaseButton");
+    clickOn("#saveButton");
+    Assertions.assertEquals("4x paprika", getIngredientTextField(0));
   }
 
   // Tests other important button with a lot of logic
@@ -96,16 +132,30 @@ public class AppTest extends ApplicationTest {
   }
 
   private String getIngredientTextField(int index) {
-    return ((TextField) controller.getIngredientViewStream().skip(3 * index + 1).findAny().get())
-        .getText();
+    return (
+      (TextField) controller
+        .getIngredientViewStream()
+        .skip(3 * index + 1)
+        .findAny()
+        .get()
+    ).getText();
   }
 
   private Button getIngredientEditButton(int index) {
-    return ((Button) controller.getIngredientViewStream().skip(3 * index + 2).findAny().get());
+    return (
+      (Button) controller
+        .getIngredientViewStream()
+        .skip(3 * index + 2)
+        .findAny()
+        .get()
+    );
   }
 
   private void createTestFile() {
     TacoCalcFileHandler fh = new TacoCalcFileHandler();
-    fh.write(new Recipe(new Ingredient("tomat", 5), new Ingredient("avocado", 2)), "testFile");
+    fh.write(
+      new Recipe(new Ingredient("tomat", 5), new Ingredient("avocado", 2)),
+      "testFile"
+    );
   }
 }
