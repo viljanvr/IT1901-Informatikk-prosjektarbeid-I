7746dda050266daaa520a1f9ef85e68ce19ca7d0
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2022-11-03 14:35:55 +0100

    Add basis for REST API

---
+++ tacocalc/restapi/src/main/java/tacocalc/restiapi/TacocalcApplication.java
@@ -0,0 +1,16 @@
+package tacocalc.restiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * REST API application using Spring.
+ */
+@SpringBootApplication
+public class TacocalcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TacocalcApplication.class, args);
+  }
+
+}
---
+++ tacocalc/restapi/src/main/java/tacocalc/restiapi/TacocalcController.java
@@ -0,0 +1,14 @@
+package tacocalc.restiapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for REST API.
+ */
+
+@RestController
+public class TacocalcController {
+
+}
