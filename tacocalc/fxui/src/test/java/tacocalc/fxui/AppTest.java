2f8c23b4c7a6d432f811c5966bd87e79264f90ad
Jonas Wolla <wolla.jonas@gmail.com>
2022-11-15 20:29:04 +0100

    Removed unnecessary imports

--- tacocalc/fxui/src/test/java/tacocalc/fxui/AppTest.java
+++ tacocalc/fxui/src/test/java/tacocalc/fxui/AppTest.java
@@ -1,14 +1,7 @@
 package tacocalc.fxui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import tacocalc.core.Ingredient;
 import tacocalc.core.Recipe;
