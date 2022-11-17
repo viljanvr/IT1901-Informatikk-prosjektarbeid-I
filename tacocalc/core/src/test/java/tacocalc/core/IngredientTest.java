cbf4828f9d74f7cce48eceee57e80dcc667241e5
Jonas Wolla <wolla.jonas@gmail.com>
2022-11-17 10:34:10 +0100

    Replaced various assertions.* imports with one assertions import

--- tacocalc/core/src/test/java/tacocalc/core/IngredientTest.java
+++ tacocalc/core/src/test/java/tacocalc/core/IngredientTest.java
@@ -1,10 +1,6 @@
 package tacocalc.core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class IngredientTest {
@@ -13,52 +9,52 @@ public class IngredientTest {
   @Test
   public void testIngredientConstructor() {
     Ingredient I1 = new Ingredient("kjøtt", 200.0, 400.0, "stk");
-    assertEquals("kjøtt", I1.getName());
-    assertEquals(200, I1.getPerPersonAmount());
-    assertEquals(400, I1.getRoundUpTo());
-    assertEquals("stk", I1.getMeasuringUnit());
+    Assertions.assertEquals("kjøtt", I1.getName());
+    Assertions.assertEquals(200, I1.getPerPersonAmount());
+    Assertions.assertEquals(400, I1.getRoundUpTo());
+    Assertions.assertEquals("stk", I1.getMeasuringUnit());
   }
 
   @Test
   public void testPerPersonIngredientAmount() {
     Ingredient I1 = new Ingredient("agurk", 3.0, "stk");
-    assertEquals(3, I1.getPerPersonAmount());
+    Assertions.assertEquals(3, I1.getPerPersonAmount());
     I1.setPerPersonAmount(4.0);
-    assertEquals(4, I1.getPerPersonAmount());
+    Assertions.assertEquals(4, I1.getPerPersonAmount());
   }
 
   @Test
   public void testToStringmethods() {
     Ingredient I1 = new Ingredient("tomat", 4.0, "stk");
-    assertEquals("[ ]: 4 stk tomat", I1.toString());
+    Assertions.assertEquals("[ ]: 4 stk tomat", I1.toString());
     Recipe r = new Recipe(I1);
     r.setBought("tomat", true);
-    assertEquals("[x]: 4 stk tomat\n", r.toString());
+    Assertions.assertEquals("[x]: 4 stk tomat\n", r.toString());
   }
 
   @Test
   public void testIngredientsBought() {
     Ingredient I2 = new Ingredient("ost", 1.0, "stk");
-    assertFalse(I2.getBought());
+    Assertions.assertFalse(I2.getBought());
     I2.setBought(true);
-    assertTrue(I2.getBought());
+    Assertions.assertTrue(I2.getBought());
   }
 
   @Test
   public void testGetTotalAmount() {
     Ingredient I1 = new Ingredient("ost", 1.0, "stk");
-    assertEquals(4, I1.getTotalAmount(4));
+    Assertions.assertEquals(4, I1.getTotalAmount(4));
     Ingredient I2 = new Ingredient("agurk", 0.20, 0.5, "stk");
-    assertEquals(1.5, I2.getTotalAmount(7));
+    Assertions.assertEquals(1.5, I2.getTotalAmount(7));
   }
 
   @Test
   public void testThrows() {
     Ingredient I1 = new Ingredient("agurk", 5.0, "stk");
-    assertThrows(IllegalArgumentException.class, () -> {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
       I1.setPerPersonAmount(-1.0);
     });
-    assertThrows(IllegalArgumentException.class, () -> {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {
       new Ingredient("agurk", 4.0, -1.0, "stk");
     });
   }
